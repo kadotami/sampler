5597a9e39d9ff60814a3b545f5ca026b27cc5c3d
kadotami <mondamin.kqfb@gmail.com>
2020-05-05 14:16:48 +0900

    サンプル完了

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,20 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flauto.dart';
+import 'package:flutter_sound/flutter_sound_player.dart';
+import 'package:flutter_sound/flutter_sound_recorder.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Sampler'),
+      ),
       body: HomeContent(),
     );
   }
@@ -18,13 +29,172 @@ class _HomeContentState extends State<HomeContent> {
   @override
   void initState() {
     super.initState();
-    loadSound();
+    _init();
   }
-
-  Future loadSound() async {}
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: [
+        audioButton(trackNum: '1'),
+        audioButton(trackNum: '2'),
+        audioButton(trackNum: '3'),
+        audioButton(trackNum: '4'),
+      ],
+    );
+  }
+
+  _init() async {}
+}
+
+class audioButton extends StatefulWidget {
+  audioButton({this.trackNum});
+  final String trackNum;
+  @override
+  _AudioButtonState createState() => _AudioButtonState();
+}
+
+class _AudioButtonState extends State<audioButton> {
+  bool _isRecording = false;
+  bool _isExsistFile = false;
+  FlutterSoundPlayer playerModule;
+  FlutterSoundRecorder recorderModule;
+  String audioFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  _init() async {
+    playerModule = await FlutterSoundPlayer().initialize();
+    recorderModule = await FlutterSoundRecorder().initialize();
+    audioFilePath = '/sampler_audio_';
+    Directory appDocDirectory;
+    if (Platform.isIOS) {
+      appDocDirectory = await getApplicationDocumentsDirectory();
+    } else {
+      appDocDirectory = await getExternalStorageDirectory();
+    }
+    audioFilePath = appDocDirectory.path + audioFilePath + widget.trackNum;
+    _isExsistFile = await fileExists();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: createWidget());
+  }
+
+  Widget createWidget() {
+    if (_isExsistFile && _isRecording) {
+      return new GestureDetector(
+          onTapUp: (TapUpDetails details) {
+            _stopRecord();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.blue[300],
+                  Colors.blue[500],
+                  Colors.blue[700],
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: const Text(
+              'Recording...',
+              style: TextStyle(color: Colors.white),
+            ),
+          ));
+    } else if (_isExsistFile) {
+      return new GestureDetector(
+          onTapDown: (TapDownDetails details) {
+            _play();
+          },
+          onTapUp: (TapUpDetails details) {
+            _stopPlay();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.orange[300],
+                  Colors.orange[500],
+                  Colors.orange[700],
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: const Text(
+              'Play',
+              style: TextStyle(color: Colors.white),
+            ),
+          ));
+    } else {
+      return new GestureDetector(
+          onTapDown: (TapDownDetails details) {
+            _record();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Colors.blue[300],
+                  Colors.blue[500],
+                  Colors.blue[700],
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: const Text(
+              'Record',
+              style: TextStyle(color: Colors.white),
+            ),
+          ));
+    }
+  }
+
+  Future<bool> fileExists() async {
+    return await File(audioFilePath).exists();
+  }
+
+  _record() async {
+    String path = await recorderModule.startRecorder(
+      uri: audioFilePath,
+      codec: t_CODEC.CODEC_AAC,
+    );
+    print('startRecorder: $path');
+    this.setState(() {
+      this._isRecording = true;
+    });
+  }
+
+  _stopRecord() async {
+    String result = await recorderModule.stopRecorder();
+    print('stopRecorder: $result');
+    this.setState(() {
+      this._isRecording = false;
+    });
+  }
+
+  _play() async {
+    String path = await playerModule
+        .startPlayer(audioFilePath, codec: t_CODEC.CODEC_AAC, whenFinished: () {
+      print('Play finished');
+      setState(() {});
+    });
+    print('startPlayer: $path');
+  }
+
+  _stopPlay() async {
+    String result = await playerModule.stopPlayer();
+    print('stopPlayer: $result');
   }
 }
